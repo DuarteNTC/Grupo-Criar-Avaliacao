a5f8990e0b1e0b6bb88b5dd993ba471f857e30a8
Guilherme Duarte <guiduarty22@gmail.com>
2023-07-11 01:49:32 -0300

    06 - Tratativa da Imutabilidade da lista de dados

--- KartRaceAnalyzer/main.cs
+++ KartRaceAnalyzer/main.cs
@@ -6,24 +6,9 @@ using System.Linq;
 
 namespace KartRaceAnalyzer
 {
-    public class Piloto
-    {
-        public string Codigo { get; set; }
-        public string Nome { get; set; }
-        public List<Volta> Voltas { get; set; }
+    public record Piloto(string Codigo, string Nome, IReadOnlyList<Volta> Voltas);
 
-        public Piloto()
-        {
-            Voltas = new List<Volta>();
-        }
-    }
-
-    public class Volta
-    {
-        public int Numero { get; set; }
-        public TimeSpan Tempo { get; set; }
-        public double VelocidadeMedia { get; set; }
-    }
+    public record Volta(int Numero, TimeSpan Tempo, double VelocidadeMedia);
 
     public class KartRaceAnalyzer
     {
@@ -32,36 +17,27 @@ namespace KartRaceAnalyzer
             // Ler o log de corrida de um arquivo
             List<string> linhasLog = File.ReadAllLines("log_corrida.txt").Skip(1).ToList();
 
-            // Criar uma lista de pilotos
-            List<Piloto> pilotos = new List<Piloto>();
-
-            foreach (string linha in linhasLog)
-            {
-                string[] dados = linha.Split(' ');
-
-                string hora = dados[0];
-                string codigo = dados[1];
-                string nome = dados[3];
-                int numeroVolta = int.Parse(dados[4]);
-
-                // Rever o CultureInfo.InvariantCulture
-                TimeSpan tempoVolta = TimeSpan.ParseExact(dados[5], @"m\:ss\.fff", CultureInfo.InvariantCulture);
-
-                double velocidadeMedia = double.Parse(dados[6], CultureInfo.InvariantCulture);
-
-                // Verificar se o piloto já existe na lista, caso contrário, adicioná-lo
-                Piloto piloto = pilotos.FirstOrDefault(x => x.Codigo == codigo);
-
-                if (piloto == null)
+            // Criar uma lista de pilotos imutável
+            List<Piloto> pilotos = linhasLog
+                .Select(linha =>
                 {
-                    piloto = new Piloto { Codigo = codigo, Nome = nome };
-                    pilotos.Add(piloto);
-                }
+                    string[] dados = linha.Split(' ');
 
-                // Adicionar a volta ao piloto
-                Volta volta = new Volta { Numero = numeroVolta, Tempo = tempoVolta, VelocidadeMedia = velocidadeMedia };
-                piloto.Voltas.Add(volta);
-            }
+                    string codigo = dados[1];
+                    string nome = dados[3];
+                    int numeroVolta = int.Parse(dados[4]);
+                    TimeSpan tempoVolta = TimeSpan.ParseExact(dados[5], @"m\:ss\.fff", CultureInfo.InvariantCulture);
+                    double velocidadeMedia = double.Parse(dados[6], CultureInfo.InvariantCulture);
+
+                    Volta volta = new Volta(numeroVolta, tempoVolta, velocidadeMedia);
+
+                    return (codigo, nome, volta);
+                })
+                .GroupBy(x => x.codigo)
+                .Select(x => new Piloto(x.Key, x.First().nome, x.Select(x => x.volta).ToList()))
+                .ToList();
+
+            // Restante do código permanece igual
 
             // Encontrar a melhor volta de cada piloto
             Console.WriteLine("Melhor volta de cada piloto:");
@@ -86,7 +62,7 @@ namespace KartRaceAnalyzer
             }
             Console.WriteLine();
 
-             // Descobrir quanto tempo cada piloto chegou após o vencedor
+            // Descobrir quanto tempo cada piloto chegou após o vencedor
             Console.WriteLine("Tempo que cada piloto chegou após o vencedor:");
             Piloto vencedor = pilotos.OrderBy(x => x.Voltas.Count).First();
             TimeSpan tempoVencedor = vencedor.Voltas.Sum(x => x.Tempo);
